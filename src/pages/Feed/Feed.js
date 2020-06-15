291cb511feb9dd10aa341e8eb67ebe477b3bd2c9
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-15 08:55:04 -0700

    Add auth middleware to all routes (#392)

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -135,6 +135,9 @@ class Feed extends Component {
     fetch(url, {
       method,
       body: formData,
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
     })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
@@ -185,7 +188,12 @@ class Feed extends Component {
 
   deletePostHandler = (postId) => {
     this.setState({ postsLoading: true });
-    fetch(`http://localhost:8080/feed/post/${postId}`, { method: 'DELETE' })
+    fetch(`http://localhost:8080/feed/post/${postId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
